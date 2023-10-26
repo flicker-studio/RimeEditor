eb8e2f493b3da7dec796ed9f297a809b11b714dd
AvA <Morsiusiurandum@outlook.com>
2023-10-27 01:19:26 +0800

    test: Add test for the Flag and passed

---
+++ moon-dev/Assets/Scripts/Test/Extension/Type/FlagTest.cs
@@ -0,0 +1,30 @@
+﻿using Moon.Extension.Type;
+using NUnit.Framework;
+
+namespace Test.Extension.Type
+{
+    [Author("Mors")]
+    internal class FlagTest
+    {
+        internal static Flag[] FlagDataSource =
+        {
+            new() { SetFlag = true },
+            new() { SetFlag = false }
+        };
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void SetFlagTest(bool source)
+        {
+            var flag = new Flag { SetFlag = source };
+            Assert.AreEqual(source, flag.GetFlag);
+        }
+
+        [TestCaseSource(nameof(FlagDataSource))]
+        public void GetFlagTest(Flag flag)
+        {
+            _ = flag.GetFlag;
+            Assert.IsFalse(flag.GetFlag);
+        }
+    }
+}
