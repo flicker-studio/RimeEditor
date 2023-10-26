a0e02e8ca41975657eeb63ce58efd26059fc873f
AvA <Morsiusiurandum@outlook.com>
2023-10-27 00:32:35 +0800

    refactor: refactor Flag struct

---
+++ moon-dev/Assets/Scripts/Extension/Type/Flag.cs
@@ -0,0 +1,33 @@
+﻿namespace Moon.Extension.Type
+{
+    /// <summary>
+    ///    Custom value types to store special flag bits.
+    /// </summary>
+    public struct Flag
+    {
+        private bool m_flag;
+
+        /// <summary>
+        ///     Set the flag to <see langword="false" />.
+        /// </summary>
+        public bool SetFlag
+        {
+            set => m_flag = value;
+        }
+
+        /// <summary>
+        ///     Gets the current flag bit and then sets it to <see langword="false" />.
+        /// </summary>
+        public bool GetFlag
+        {
+            get
+            {
+                var tempValue = m_flag;
+
+                if (m_flag) m_flag = false;
+
+                return tempValue;
+            }
+        }
+    }
+}
--- moon-dev/Assets/Scripts/Struct/FlagProperty.cs
+++ moon-dev/Assets/Scripts/Struct/FlagProperty.cs
@@ -1,17 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// <see cref="FlagProperty"/> has been deprecated and <see cref="Moon.Extension.Type.Flag"/> is being considered instead.
+/// </summary>
+[Obsolete("Struct.FlagProperty has been deprecated and Moon.Extension.Type.Flag is being considered instead.")]
 public struct FlagProperty
 {
     private bool m_flag;
 
     public bool SetFlag
     {
-        set
-        {
-            m_flag = value;
-        }
+        set { m_flag = value; }
     }
 
     public bool GetFlag
@@ -19,6 +21,7 @@ public struct FlagProperty
         get
         {
             bool tempValue = m_flag;
+
             if (m_flag)
             {
                 m_flag = false;
