53190d84c1f6ef5ec8a5920c84f38184f6ffbac1
AvA <Morsiusiurandum@outlook.com>
2023-11-04 01:15:07 +0800

    refactor: overwrite the extension method of string

---
+++ moon-dev/Assets/Scripts/Extension/Method/String.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Moon.Extension.Method
+{
+    /// <summary>
+    ///     <see langword="string" /> static extension class
+    /// </summary>
+    public static class String
+    {
+        /// <summary>
+        ///     Remove the number at the end of a string
+        /// </summary>
+        /// <remarks>
+        ///     Note! The function doesn't remove spaces!
+        /// </remarks>
+        /// <param name="input">The string you entered</param>
+        /// <returns>Strings stripped of numbers</returns>
+        public static string RemoveTrailingNumbers(this string input)
+        {
+            return Regex.Replace(input, @"\d+$", "");
+        }
+    }
+}
---
+++ moon-dev/Assets/Scripts/Test/Extension/Method/StringTest.cs
@@ -0,0 +1,18 @@
+﻿using Moon.Extension.Method;
+using NUnit.Framework;
+
+namespace Test.Extension.Method
+{
+    [Author("Mors"), TestOf(nameof(String))]
+    internal class StringTest
+    {
+        [TestCase("Object 123", ExpectedResult = "Object ")]
+        [TestCase("Object-4560", ExpectedResult = "Object-")]
+        [TestCase("Obje12ct4560", ExpectedResult = "Obje12ct")]
+        [TestCase("Objet12ct45.60", ExpectedResult = "Objet12ct45.")]
+        public string RemoveTrailingNumbersTest(string source)
+        {
+            return source.RemoveTrailingNumbers();
+        }
+    }
+}
