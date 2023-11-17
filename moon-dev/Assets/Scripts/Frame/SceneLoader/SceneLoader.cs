a4c8b97e1ab08613c068aa972143ea6c72c90061
AstoraGray <116440002+AstoraGray@users.noreply.github.com>
2023-11-17 21:42:32 +0800

    fix: 关卡编辑器更新
    - 补充漏掉的提交

---
+++ moon-dev/Assets/Scripts/Frame/SceneLoader/SceneLoader.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
+
+namespace Frame.Tool
+{
+    public class SceneLoader : Singleton<SceneLoader>
+    {
+        public async UniTask EnterScene(string sceneName)
+        {
+            await LoadScene(sceneName);
+        }
+
+        public async UniTask AddScene(string sceneName)
+        {
+            SetCurrentSceneObjectsActive(false);
+            await LoadScene(sceneName,LoadSceneMode.Additive);
+        }
+
+        public async UniTask RemoveCurrentScene()
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            
+            await SceneManager.UnloadSceneAsync(currentScene);
+            
+            SetCurrentSceneObjectsActive(true);
+        }
+        
+        private void SetCurrentSceneObjectsActive(bool value)
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            if (scene.isLoaded)
+            {
+                foreach (GameObject go in scene.GetRootGameObjects())
+                {
+                    go.SetActive(value);
+                }
+            }
+        }
+        
+        private async UniTask LoadScene(string sceneName,LoadSceneMode loadSceneMode = LoadSceneMode.Single)
+        {
+            await SceneManager.LoadSceneAsync(sceneName, loadSceneMode);
+        }
+    }
+}
