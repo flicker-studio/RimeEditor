9e35ddc04b62ac0918b475c9cd06aff16755f995
AvA <Morsiusiurandum@outlook.com>
2023-11-19 22:07:34 +0800

    feat: add a simple runtime

---
+++ moon-dev/Assets/Scripts/Runtime/System/Boot.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Moon.Runtime.System
+{
+    /// <summary>
+    ///     The population class of the entire game system is responsible for initialization at the lowest level.
+    /// </summary>
+    public static class Boot
+    {
+        private const string PersistenceSceneName = "Persistent";
+
+        /// <summary>
+        ///     Callback invoked when all assemblies are loaded and preloaded assets are initialized.
+        ///     At this time the objects of the first scene have not been loaded yet.
+        /// </summary>
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+        private static async void BootLoader()
+        {
+            Debug.Log("<color=green>[SYS]</color> System is Booting...");
+            await StartServiceAsync();
+        }
+
+
+        /// <summary>
+        ///     Instantiate all service classes and initialize them.
+        /// </summary>
+        private static async Task StartServiceAsync()
+        {
+            Debug.Log("<color=green>[SERVICE]</color> Initializing service...");
+
+            await Task.CompletedTask;
+
+            Debug.Log("<color=green>[SERVICE]</color> Initialization is complete.");
+        }
+
+#if UNITY_EDITOR
+        [InitializeOnLoadMethod]
+        private static void PreCheck()
+        {
+            var sceneCount = SceneManager.sceneCountInBuildSettings;
+            var scenePath = new string[sceneCount];
+
+            for (var i = 0; i < sceneCount; i++) scenePath[i] = Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+
+            var sceneName = scenePath.Select(path => path[(path.LastIndexOf('/') + 1)..]).ToArray();
+
+            if (!sceneName.Contains(PersistenceSceneName))
+                throw new Exception($"{PersistenceSceneName} scene is lose!");
+        }
+#endif
+    }
+}
---
+++ moon-dev/Assets/Scripts/Runtime/System/ServiceBase.cs
@@ -0,0 +1,82 @@
+﻿namespace Moon.Runtime.System
+{
+    /// <summary>
+    ///     Provides a base class for the service that will exist as part of the service application.
+    ///     When you create a new service class, you must derive from <c>ServiceBase</c>.
+    /// </summary>
+    public abstract class ServiceBase
+    {
+        /// <summary>
+        ///     Specifies the action to take when the service starts.
+        /// </summary>
+        /// <remarks>
+        ///     When the Service Control Manager (SCM) sends a Start command to a service,
+        ///     or when the operating system starts (for services that start automatically).
+        /// </remarks>
+        protected virtual void OnStart()
+        {
+            ;
+        }
+
+        /// <summary>
+        ///     Specifies the action to take when the service is paused.
+        /// </summary>
+        /// <remarks>
+        ///     This method is executed when the Service Control Manager (SCM) sends a Pause command to the service.
+        /// </remarks>
+        protected virtual void OnPause()
+        {
+            ;
+        }
+
+        /// <summary>
+        ///     Specifying the action to take when normal functionality resumes after the service is paused.
+        /// </summary>
+        /// <remarks>
+        ///     Runs when the Service Control Manager (SCM) sends the Continue command to a service.
+        /// </remarks>
+        protected virtual void OnContinue()
+        {
+            ;
+        }
+
+        /// <summary>
+        ///     Specifying the action to take when normal functionality resumes after the service is paused.
+        /// </summary>
+        /// <remarks>
+        ///     Runs when the Service Control Manager (SCM) sends the Continue command to a service,
+        /// </remarks>
+        protected virtual void OnStop()
+        {
+            ;
+        }
+
+        /// <summary>
+        ///     Stop executing the service.
+        /// </summary>
+        /// <remarks>
+        ///     The Stop method sets the service state to indicate that Stop is in a pending state
+        ///     and calls the <see cref="OnStop" /> method.
+        /// </remarks>
+        public void Stop()
+        {
+            OnStop();
+        }
+
+        /// <summary>
+        ///     Dispose of occupied resources.
+        /// </summary>
+        /// <param name="all">
+        ///     To release all resources, then <see langword="true" />;
+        ///     If only temporary is released, it is <see langword="false" />.
+        /// </param>
+        protected abstract void Dispose(bool all);
+
+        /// <summary>
+        ///     Provides the main entry point for the service executable.
+        /// </summary>
+        public static void Run(ServiceBase service)
+        {
+        }
+    }
+}
