5ae050fe3c5257a7c417128e6c00bf51aa9375c7
NicoIer <91652330+NicoIer@users.noreply.github.com>
2023-11-24 17:28:34 +0800

    feat: 机器人巡逻状态

---
+++ moon-dev/Assets/Scripts/AI/RobotModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityToolkit;
+
+namespace Moon
+{
+#if UNITY_EDITOR
+    [Serializable]
+#endif
+    public class RobotModel : Model<RobotModel>
+    {
+        public Vector2 facingDir => facing == FacingDirection2D.Left ? Vector2.left : Vector2.right;
+
+        public FacingDirection2D facing;
+    }
+}
