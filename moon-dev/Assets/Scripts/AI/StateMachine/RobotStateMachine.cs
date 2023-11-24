c3854be93f691c0ebb5584d89f94d226400830a0
NicoIer <91652330+NicoIer@users.noreply.github.com>
2023-11-24 17:28:34 +0800

    feat: 机器人巡逻状态

---
+++ moon-dev/Assets/Scripts/AI/StateMachine/RobotStateMachine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityToolkit;
+
+namespace Moon
+{
+    public sealed class RobotStateMachine : StateMachine<Robot>
+    {
+        public RobotStateMachine(Robot owner) : base(owner)
+        {
+        }
+    }
+}
---
+++ moon-dev/Assets/Scripts/AI/StateMachine/State/PatrolState.cs
@@ -0,0 +1,17 @@
+﻿using UnityToolkit;
+
+namespace Moon
+{
+    /// <summary>
+    /// 巡逻状态
+    /// </summary>
+    public class PatrolState : State<Robot>
+    {
+        public override void OnUpdate(Robot owner)
+        {
+            float patrolSpeed = owner.config.patrolSpeed;
+            float facing = owner.model.facing == FacingDirection2D.Left ? -1 : 1;
+            owner.rb2D.velocity = new UnityEngine.Vector2(patrolSpeed, 0) * facing;
+        }
+    }
+}
