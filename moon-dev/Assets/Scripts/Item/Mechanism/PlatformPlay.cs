25543e89b8a771a4d904c6dd54ec37d6126e2166
AstoraGray <116440002+AstoraGray@users.noreply.github.com>
2023-12-14 13:30:52 +0800

    feat: 关卡编辑器更新
    - 完成了Inspector面板的整体框架
    - 增加了CanPush选项

--- moon-dev/Assets/Scripts/Item/Mechanism/PlatformPlay.cs
+++ moon-dev/Assets/Scripts/Item/Mechanism/PlatformPlay.cs
@@ -1,5 +1,8 @@
 using Frame.Static.Global;
 using Frame.Tool.Pool;
+using Data.ScriptableObject;
+using Frame.Static.Global;
+using Frame.Tool.Pool;
 using Item;
 using UnityEngine;
 
@@ -8,10 +11,21 @@ public class PlatformPlay : ItemPlay
     public bool CanCopy;
     public bool CanPush;
     private GameObject m_rigidbodyParent;
+    private GameObject m_rigidbodyPrefab;
+
+    private void Start()
+    {
+        PrefabFactory m_prefabFactory = Resources.Load<PrefabFactory>("GlobalSettings/PrefabFactory");
+        m_rigidbodyPrefab = m_prefabFactory.RIGIDBODY_PARENT;
+    }
+
+    public bool CanCopy;
+    public bool CanPush;
+    private GameObject m_rigidbodyParent;
     private Renderer m_renderer;
     private Material m_originMaterial;
-    [SerializeField]private GameObject m_rigidbodyPrefab;
-    [SerializeField]private Material m_canCopymaterial;
+    [SerializeField] private GameObject m_rigidbodyPrefab;
+    [SerializeField] private Material m_canCopymaterial;
 
     private void Start()
     {
@@ -21,43 +35,11 @@ public class PlatformPlay : ItemPlay
 
     public override void Play()
     {
-        if (CanPush)
-        {
-            m_rigidbodyParent = ObjectPool.Instance.OnTake(m_rigidbodyParent,m_rigidbodyPrefab);
-            m_rigidbodyParent.transform.parent = transform.parent;
-            transform.parent = m_rigidbodyParent.transform;
-            if (!gameObject.name.Contains(GlobalSetting.ObjNameTag.RIGIDBODY_TAG))
-            {
-                gameObject.name += GlobalSetting.ObjNameTag.RIGIDBODY_TAG;
-            }
-        }
 
-        if (CanCopy)
-        {
-            if (!gameObject.name.Contains(GlobalSetting.ObjNameTag.CAN_COPY_TAG))
-            {
-                gameObject.name += GlobalSetting.ObjNameTag.CAN_COPY_TAG;
-            }
-
-            m_renderer.material = m_canCopymaterial;
-        }
     }
 
     public override void Stop()
     {
-        if (CanPush)
-        {
-            ObjectPool.Instance.OnRelease(m_rigidbodyParent);
-        }
-        if (gameObject.name.Contains(GlobalSetting.ObjNameTag.RIGIDBODY_TAG))
-        {
-            gameObject.name = gameObject.name.Replace(GlobalSetting.ObjNameTag.RIGIDBODY_TAG, string.Empty);
-        }
-        if (gameObject.name.Contains(GlobalSetting.ObjNameTag.CAN_COPY_TAG))
-        {
-            gameObject.name = gameObject.name.Replace(GlobalSetting.ObjNameTag.CAN_COPY_TAG, string.Empty);
-        }
 
-        m_renderer.material = m_originMaterial;
     }
 }
