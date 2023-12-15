059df99a60bda34752961a9586207c11b737bb20
AstoraGray <116440002+AstoraGray@users.noreply.github.com>
2023-12-15 16:14:22 +0800

    feat: 关卡编辑器更新
    - 实现了CanCopy功能

--- moon-dev/Assets/Scripts/Item/Mechanism/PlatformPlay.cs
+++ moon-dev/Assets/Scripts/Item/Mechanism/PlatformPlay.cs
@@ -1,8 +1,5 @@
 using Frame.Static.Global;
 using Frame.Tool.Pool;
-using Data.ScriptableObject;
-using Frame.Static.Global;
-using Frame.Tool.Pool;
 using Item;
 using UnityEngine;
 
@@ -11,12 +8,29 @@ public class PlatformPlay : ItemPlay
     public bool CanCopy;
     public bool CanPush;
     private GameObject m_rigidbodyParent;
-    private GameObject m_rigidbodyPrefab;
+    private Renderer m_renderer;
+    private Material m_originMaterial;
+    [SerializeField] private GameObject m_rigidbodyPrefab;
+    [SerializeField] private Material m_canCopymaterial;
 
     private void Start()
     {
-        PrefabFactory m_prefabFactory = Resources.Load<PrefabFactory>("GlobalSettings/PrefabFactory");
-        m_rigidbodyPrefab = m_prefabFactory.RIGIDBODY_PARENT;
+        m_renderer = GetComponent<Renderer>();
+        m_originMaterial = m_renderer.material;
+    }
+
+    public bool CanCopy;
+    public bool CanPush;
+    private GameObject m_rigidbodyParent;
+    private Renderer m_renderer;
+    private Material m_originMaterial;
+    [SerializeField] private GameObject m_rigidbodyPrefab;
+    [SerializeField] private Material m_canCopymaterial;
+
+    private void Start()
+    {
+        m_renderer = GetComponent<Renderer>();
+        m_originMaterial = m_renderer.material;
     }
 
     public bool CanCopy;
@@ -35,11 +49,33 @@ public class PlatformPlay : ItemPlay
 
     public override void Play()
     {
-
+        if (CanPush)
+        {
+            m_rigidbodyParent = ObjectPool.Instance.OnTake(m_rigidbodyParent, m_rigidbodyPrefab);
+            m_rigidbodyParent.transform.parent = transform.parent;
+            transform.parent = m_rigidbodyParent.transform;
+            if (!gameObject.name.Contains(GlobalSetting.ObjNameTag.RIGIDBODY_TAG))
+            {
+                gameObject.name += GlobalSetting.ObjNameTag.RIGIDBODY_TAG;
+            }
+        }
     }
 
     public override void Stop()
     {
+        if (CanPush)
+        {
+            ObjectPool.Instance.OnRelease(m_rigidbodyParent);
+        }
+        if (gameObject.name.Contains(GlobalSetting.ObjNameTag.RIGIDBODY_TAG))
+        {
+            gameObject.name = gameObject.name.Replace(GlobalSetting.ObjNameTag.RIGIDBODY_TAG, string.Empty);
+        }
+        if (gameObject.name.Contains(GlobalSetting.ObjNameTag.CAN_COPY_TAG))
+        {
+            gameObject.name = gameObject.name.Replace(GlobalSetting.ObjNameTag.CAN_COPY_TAG, string.Empty);
+        }
 
+        m_renderer.material = m_originMaterial;
     }
 }
