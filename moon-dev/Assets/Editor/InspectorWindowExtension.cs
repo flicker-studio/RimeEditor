34328a09872b06a5fa18068ef2fdc877f9fb9c7e
AvA <Morsiusiurandum@outlook.com>
2024-01-23 20:53:37 +0800

    perf: reduce some packages

--- moon-dev/Assets/Editor/InspectorWindowExtension.cs
+++ moon-dev/Assets/Editor/InspectorWindowExtension.cs
@@ -1,11 +1,9 @@
 using System;
+using System.IO;
 using Data.ScriptableObject;
 using Frame.Static.Global;
-using Slicer;
-using Unity.VisualScripting;
-using UnityEngine;
 using UnityEditor;
-using System.IO;
+using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace WindowExtension
@@ -14,51 +12,67 @@ namespace WindowExtension
     public class PolygonCollider2DEditor : UnityEditor.Editor
     {
         private UnityEditor.Editor defaultEditor;
-        
-        void OnEnable()
+
+        private void OnEnable()
         {
-            defaultEditor = CreateEditor(targets, System.Type.GetType("UnityEditor.PolygonCollider2DEditor, UnityEditor"));
+            defaultEditor = CreateEditor(targets, Type.GetType("UnityEditor.PolygonCollider2DEditor, UnityEditor"));
         }
-        
-        void OnDisable()
+
+        private void OnDisable()
         {
             DestroyImmediate(defaultEditor);
         }
+
         public override void OnInspectorGUI()
         {
-            if(target == null) return;
+            if (target == null)
+            {
+                return;
+            }
+
             defaultEditor.OnInspectorGUI();
             DrawDefaultInspector();
-            
-            PolygonCollider2D collider = (PolygonCollider2D)target;
-            if(GUILayout.Button("CreateColliderAndSetLayer")) 
+
+            var collider = (PolygonCollider2D)target;
+
+            if (GUILayout.Button("CreateColliderAndSetLayer"))
             {
-                CreateColliderAndSetLayer(collider); 
+                CreateColliderAndSetLayer(collider);
             }
-            if(GUILayout.Button("CreateRigidbody")) 
+
+            if (GUILayout.Button("CreateRigidbody"))
             {
-                CreateRigidbody(); 
+                CreateRigidbody();
             }
         }
 
-        void CreateColliderAndSetLayer(PolygonCollider2D collider)
+        private void CreateColliderAndSetLayer(PolygonCollider2D collider)
         {
-            target.GetComponent<MeshFilter>().sharedMesh.CreatePolygonCollider(collider);
-            target.GameObject().layer = GlobalSetting.LayerMasks.GROUND;
+            throw new Exception("The methods do not exist!");
+
+            // TODO:Fix compilation errors
+            //  target.GetComponent<MeshFilter>().sharedMesh.CreatePolygonCollider(collider);
+            //  target.GameObject().layer = GlobalSetting.LayerMasks.GROUND;
             Undo.RegisterCreatedObjectUndo(target, "CreateColliderAndSetLayer " + target.name);
         }
 
-        void CreateRigidbody()
+        private void CreateRigidbody()
         {
-            GameObject rigidbodyParentPrefab = Resources.Load<PrefabFactory>("GlobalSettings/PrefabFactory").RIGIDBODY_PARENT;
-            GameObject rigidbodyParent = GameObject.Instantiate(rigidbodyParentPrefab);
-            rigidbodyParent.transform.parent = target.GameObject().transform.parent;
-            target.GameObject().transform.parent = rigidbodyParent.transform;
-            rigidbodyParent.name = rigidbodyParentPrefab.name + Random.Range(10000,1000000);
+            throw new Exception("The methods do not exist!");
+
+            var rigidbodyParentPrefab = Resources.Load<PrefabFactory>("GlobalSettings/PrefabFactory").RIGIDBODY_PARENT;
+            var rigidbodyParent = Instantiate(rigidbodyParentPrefab);
+
+            // TODO:Fix compilation errors
+            // rigidbodyParent.transform.parent = target.GameObject().transform.parent;
+            // target.GameObject().transform.parent = rigidbodyParent.transform;
+            rigidbodyParent.name = rigidbodyParentPrefab.name + Random.Range(10000, 1000000);
+
             if (!target.name.Contains(GlobalSetting.ObjNameTag.RIGIDBODY_TAG))
             {
                 target.name += GlobalSetting.ObjNameTag.RIGIDBODY_TAG;
             }
+
             Undo.RegisterCreatedObjectUndo(target, "CreateRigidbody " + target.name);
         }
     }
@@ -68,49 +82,57 @@ namespace WindowExtension
     {
         private UnityEditor.Editor defaultEditor;
 
-        void OnEnable()
+        private void OnEnable()
         {
-            defaultEditor = CreateEditor(targets, System.Type.GetType("UnityEditor.RectTransformEditor, UnityEditor"));
+            defaultEditor = CreateEditor(targets, Type.GetType("UnityEditor.RectTransformEditor, UnityEditor"));
         }
 
         public override void OnInspectorGUI()
         {
-            if(target == null) return;
+            if (target == null)
+            {
+                return;
+            }
+
             defaultEditor.OnInspectorGUI();
 
             if (GUILayout.Button("Copy Path"))
             {
-                RectTransform rectTransform = (RectTransform)target;
-                string path = GeneratePath(rectTransform);
+                var rectTransform = (RectTransform)target;
+                var path = GeneratePath(rectTransform);
                 EditorGUIUtility.systemCopyBuffer = path;
+
                 EditorWindow.focusedWindow.ShowNotification
                     (new GUIContent("Copied: " + path));
             }
         }
 
-        string GeneratePath(Transform transform)
+        private string GeneratePath(Transform transform)
         {
-            string path = transform.name;
+            var path = transform.name;
+
             while (transform.parent != null && transform.parent.GetComponent<RectTransform>() != null)
             {
                 transform = transform.parent;
                 path = transform.name + "/" + path;
             }
 
-            int firstSlash = path.IndexOf('/');
+            var firstSlash = path.IndexOf('/');
+
             if (firstSlash >= 0)
             {
                 path = path.Substring(firstSlash + 1);
             }
+
             return path;
         }
 
-        void OnDisable()
+        private void OnDisable()
         {
             DestroyImmediate(defaultEditor);
         }
     }
-    
+
     [CustomEditor(typeof(ItemProduct))]
     public class ItemProductEditor : UnityEditor.Editor
     {
@@ -118,17 +140,27 @@ namespace WindowExtension
         {
             DrawDefaultInspector();
 
-            ItemProduct itemProduct = (ItemProduct)target;
+            var itemProduct = (ItemProduct)target;
+
             if (GUILayout.Button("Set Prefab Name"))
             {
-                if(itemProduct.ItemObject == null) return;
+                if (itemProduct.ItemObject == null)
+                {
+                    return;
+                }
+
                 AssetDatabase.RenameAsset(AssetDatabase.GetAssetPath(itemProduct.ItemObject)
                     , itemProduct.Name);
+
                 AssetDatabase.Refresh();
             }
+
             if (GUILayout.Button("Generate Image"))
             {
-                if(itemProduct.ItemObject == null) return;
+                if (itemProduct.ItemObject == null)
+                {
+                    return;
+                }
 
                 if (itemProduct.ItemIcon != null)
                 {
@@ -141,27 +173,29 @@ namespace WindowExtension
                         return;
                     }
                 }
-                
-                Texture2D preview = AssetPreview.GetAssetPreview(itemProduct.ItemObject);
-                
-                byte[] bytes = preview.EncodeToPNG();
 
-                string typeDirectory = Enum.GetName(typeof(ITEMTYPEENUM), itemProduct.ItemType);
-                
-                string path = Application.dataPath +
-                              $"/Resources/Items/Images/{typeDirectory}/{itemProduct.Name}" + ".png";
-                
+                var preview = AssetPreview.GetAssetPreview(itemProduct.ItemObject);
+
+                var bytes = preview.EncodeToPNG();
+
+                var typeDirectory = Enum.GetName(typeof(ITEMTYPEENUM), itemProduct.ItemType);
+
+                var path = Application.dataPath +
+                           $"/Resources/Items/Images/{typeDirectory}/{itemProduct.Name}" + ".png";
+
                 Directory.CreateDirectory(Path.GetDirectoryName(path));
-                
+
                 File.WriteAllBytes(path, bytes);
-                
+
                 AssetDatabase.Refresh();
-                
+
                 EditorApplication.update += WaitForTextureImport;
+
                 void WaitForTextureImport()
                 {
-                    Sprite sprite = Resources.Load<Sprite>($"Items/Images/{typeDirectory}/{itemProduct.Name}");
-                    if (sprite  != null)
+                    var sprite = Resources.Load<Sprite>($"Items/Images/{typeDirectory}/{itemProduct.Name}");
+
+                    if (sprite != null)
                     {
                         itemProduct.ItemIcon = sprite;
                         EditorApplication.update -= WaitForTextureImport;
@@ -170,5 +204,4 @@ namespace WindowExtension
             }
         }
     }
-
 }
